ccdc13206799f6acf5fd7cdcf70ef91b3f8951eb
Christopher Melby <melby.christopher@gmail.com>
2019-12-01 15:07:03 -0800

    character array and initital promt complete. moving on to the next promts, storage, and random generationof pw on page

---
+++ style.js
@@ -0,0 +1,26 @@
+
+// variables
+var passWd;
+var userInput = [];
+
+var characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, "!", "#", "$", "%", "&", "(", ")", "*", "+", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "^", "_", "`", "{", "|", "}", "~"];
+//27 0-26 special characters
+// 0-9
+//26 0-25 letters in the alphebet
+// document.onekeyup = function(userInput){
+passWd = alert("Lets create your password! All passwords must be between 8 & 128 characters."); {
+    if (passWd = true) {
+        var enterUc = prompt("Your password must incldue uppercase letters. Please enter below:");
+    
+    
+    }
+}
+
+// }
+
+
+
+
+
+
+
