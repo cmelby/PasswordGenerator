f568215ba2064e164d604fceac0b7dcc19a92267
Christopher Melby <melby.christopher@gmail.com>
2019-12-03 00:47:31 -0800

    If statments created, values stored in new array and .random generating a random string from that array. However its grabbing my entire array string rather than individual values. Still need to create and event as well to display the new PW in html.

--- style.js
+++ style.js
@@ -6,34 +6,92 @@ var numeric = "0123456789";
 var special = "!$^&*-=+_?";
 
 //Global varibles used to store the confirms:
-// passWd;
-upperC;
-lowerC;
-numericV;
-specialC;
-newPassWd;
+var newPassWd = [];
+
 
 //=======================User Walkthough==================================//
 //Prompts & confirms
-var passWd = prompt("How many characters do you want to be in your password?"); 
-if (passWd < 8) {
+
+pWlenght = prompt("How many characters do you want to be in your password?"); 
+if (pWlenght < 8) {
     alert("All passwords must be between 8-128 characters.");
     prompt("How many characters do you want to be in your password?");
+    
   }
-  else if (passWd > 8 );
+  else if (pWlenght > 8 ); {
 
-var lower = confirm("Click ok to confirm including upper case letters."); 
-var caps = confirm("Click ok to confirm including lower case letters."); 
-var numeric = confirm("Click ok to confirm including numeric values."); 
-var special = confirm("Click ok to confirm including special characters."); 
+  }
+upperC = confirm("Click ok to confirm including upper case letters."); 
+lowerC = confirm("Click ok to confirm including lower case letters."); 
+numericV = confirm("Click ok to confirm including numeric values."); 
+specialC = confirm("Click ok to confirm including special characters."); 
 
-if (lower === true) {
-newPassWd.push(lower);
+
+if (upperC === true) {
+   (newPassWd.push(caps));
 }
+ if (lowerC === true) {
+    (newPassWd.push(lower));
+ }
+if (numericV === true) {
+    (newPassWd.push(numeric));
+ }
+if (specialC = true) {
+    (newPassWd.push(special));
+ }
 
+var genPW = newPassWd[Math.floor(Math.random() * newPassWd.length)]; 
+
+console.log(pWlenght);
+console.log(upperC);
+console.log(lowerC);
+console.log(numericV);
+console.log(specialC);
+console.log(newPassWd);
+console.log(genPW);
+
+// old===========================================================
+// newPassWd = (upperC + lowerC + numericV + specialC );
+
+// console.log(newPassWd);
+
+
+// for (var i = 0, n = newPassWd.length; i < length; ++i) {
+// var genPW = newPassWd.charAt(Math.floor(Math.random() * newPassWd.length));
+// }
+// console.log(genPW);
 
 
 // var upperC = confirm("Click ok to confirm including upper case letters."); 
 // var lowerC = confirm("Click ok to confirm including lower case letters."); 
 // var numericV = confirm("Click ok to confirm including numeric values."); 
 // var specialC = confirm("Click ok to confirm including special characters."); 
+
+// var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+// var newPassWd = caps.concat(upperC)
+
+
+//old variables
+// var pWlenght;
+// var upperC;
+// var lowerC;
+// var numericV;
+// var specialC;
+
+//old arrays
+// var caps = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",];
+// var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+// var numeric = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+// var special = [ "!", "$", "^", "&", "*", "-", "=", "+", "_", "?" ];
+
+
+//old function 
+// function genPW() {
+ 
+//     var getPW = " ";
+
+//  for (var i = 0, n = newPassWd.length; i < length; ++i) {
+//     getPW += newPassWd.charAt(Math.floor(Math.random() * newPassWd.length));
+//     }
+//     return getPW;
+// }
